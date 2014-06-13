737b004c9bce49ac3a95820a1a0b0b1c6cf99b79
Ganesh <ganeshsurya11@gmail.com>
2014-06-13 17:32:52 +0530

    Added layout

--- test.js
+++ test.js
@@ -1,1 +1,58 @@
-angular.module('mySuperApp', []);
+ 
+app = angular.module('todo', ['ionic'])
+
+app.controller('appCtrl', ['$scope', function($scope){
+	$scope.Title = "Intelligynt Security Service";	
+}])
+
+app.controller('tasksCtrl', ['$scope', function($scope){
+	 $scope.tasks = [
+		{ title: 'Collect coins' },
+		{ title: 'Eat mushrooms' },
+		{ title: 'Get high enough to grab the flag' },
+		{ title: 'Find the Princess' },
+		{ title: 'Collect coins' },
+		{ title: 'Eat mushrooms' },
+		{ title: 'Get high enough to grab the flag' },
+		{ title: 'Find the Princess' },
+		{ title: 'Collect coins' },
+		{ title: 'Eat mushrooms' },
+		{ title: 'Get high enough to grab the flag' },
+		{ title: 'Find the Princess' },
+		{ title: 'Collect coins' },
+		{ title: 'Eat mushrooms' },
+		{ title: 'Get high enough to grab the flag' },
+		{ title: 'Find the Princess' },
+		{ title: 'Collect coins' },
+		{ title: 'Eat mushrooms' },
+		{ title: 'Get high enough to grab the flag' },
+		{ title: 'Find the Princess' },
+		{ title: 'Collect coins' },
+		{ title: 'Eat mushrooms' },
+		{ title: 'Get high enough to grab the flag' },
+		{ title: 'Find the Princess' }
+  	];
+}])
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
